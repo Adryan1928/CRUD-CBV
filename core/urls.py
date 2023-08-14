43e032ded71003a947ab34b4c8264f51bd39860d
Adryan1928 <adryanoliveira1928@gmail.com>
2023-08-14 14:44:06 -0300

    Criei a API da aplicação

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from api.views import ClienteViewSet
+
+router = routers.DefaultRouter()
+router.register('clientes', ClienteViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('exemplo.urls'))
-]
+] + [path('api/', include(router.urls))]

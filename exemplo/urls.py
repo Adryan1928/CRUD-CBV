a99d51fa8f48d29c74161b2a44016d992f9afc21
Adryan1928 <adryanoliveira1928@gmail.com>
2023-08-14 14:30:28 -0300

    Terminei o CRUD

--- exemplo/urls.py
+++ exemplo/urls.py
@@ -5,5 +5,8 @@ app_name = 'exemplo'
 
 urlpatterns = [
     path('list/', view=views.ClienteList.as_view(), name='list'),
-    path('create/', view=views.ClienteCreate.as_view(), name='create')
+    path('create/', view=views.ClienteCreate.as_view(), name='create'),
+    path('update/<int:pk>/', view=views.ClienteUpdate.as_view(), name='update'),
+    path('detail/<int:pk>/', view=views.ClienteDetail.as_view(), name='detail'),
+    path('delete/<int:pk>/', view=views.ClienteDelete.as_view(), name='delete')
 ]
--- exemplo/views.py
+++ exemplo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from .models import Cliente
 
 class ClienteList(ListView):
@@ -12,3 +12,20 @@ class ClienteCreate(CreateView):
     fields = '__all__'
     success_url = '/clientes/list/'
     template_name = 'cliente_create.html'
+
+class ClienteUpdate(UpdateView):
+    model = Cliente
+    fields = '__all__'
+    success_url = '/clientes/list/'
+    template_name = 'cliente_update.html'
+
+class ClienteDetail(DetailView):
+    model = Cliente
+    queryset = Cliente.objects.all()
+    template_name = 'cliente_detail.html'
+
+class ClienteDelete(DeleteView):
+    model = Cliente
+    queryset = Cliente.objects.all()
+    success_url = '/clientes/list/'
+    template_name = 'cliente_delete.html'

4e174f4b5b659ed8ff11e8ec5fc8a842334a4dcd
Adryan1928 <adryanoliveira1928@gmail.com>
2023-08-20 14:23:46 -0300

    Criei um login

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from api.views import ClienteViewSet
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register('clientes', ClienteViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('clientes/', include('exemplo.urls'))
+    path('', view=TemplateView.as_view(template_name='home.html'), name='home'),
+    path('clientes/', include('exemplo.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + [path('api/', include(router.urls))]
--- exemplo/views.py
+++ exemplo/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from .models import Cliente
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ClienteList(ListView):
+class ClienteList(LoginRequiredMixin, ListView):
     model = Cliente
     queryset = Cliente.objects.all()
     template_name = 'cliente_list.html'
 
-class ClienteCreate(CreateView):
+class ClienteCreate(LoginRequiredMixin, CreateView):
     model = Cliente
     fields = '__all__'
     success_url = '/clientes/list/'
     template_name = 'cliente_create.html'
 
-class ClienteUpdate(UpdateView):
+class ClienteUpdate(LoginRequiredMixin, UpdateView):
     model = Cliente
     fields = '__all__'
     success_url = '/clientes/list/'
     template_name = 'cliente_update.html'
 
-class ClienteDetail(DetailView):
+class ClienteDetail(LoginRequiredMixin, DetailView):
     model = Cliente
     queryset = Cliente.objects.all()
     template_name = 'cliente_detail.html'
 
-class ClienteDelete(DeleteView):
+class ClienteDelete(LoginRequiredMixin, DeleteView):
     model = Cliente
     queryset = Cliente.objects.all()
     success_url = '/clientes/list/'

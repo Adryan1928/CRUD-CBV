e128e7ec43b0885c229bceeff8a539be676a77ca
Adryan1928 <adryanoliveira1928@gmail.com>
2023-09-07 12:55:07 -0300

    model de suporte usando OneToOneField

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Suporte(models.Model):
+    name = models.CharField(max_length=30)
+    usuario = models.OneToOneField(User, on_delete=models.SET_NULL, blank=True, null=True) #models.SET_NULL models.PROTECT
+
+    def __str__(self):
+        return self.name
